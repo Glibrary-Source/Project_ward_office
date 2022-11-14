72f158a6e31de3b1a36fd2014e09ef3bb19226c0
KGD <kimgyudo93@gmail.com>
2022-11-14 15:51:13 +0900

    Complite Navgraph

--- TastyWardOfficeSecond/TastyWardOffice/app/src/main/java/com/example/tastywardoffice/MainActivity.kt
+++ TastyWardOfficeSecond/TastyWardOffice/app/src/main/java/com/example/tastywardoffice/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.tastywardoffice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.tastywardoffice.databinding.ActivityMainBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,31 +25,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initNavigationBar()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
+
+//        initNavigationBar()
     }
+
+
+
+
 
     //하단 네비게이션바에서 프래그먼트 이동 코드
-    fun initNavigationBar() {
-        binding.bottomNavigation.run {
-            setOnItemSelectedListener {
-                when (it.itemId) {
-                    R.id.item_list -> {
-                        changeFragment(restaurant_list())
-                    }
-                    R.id.item_map -> {
-                        changeFragment(google_map())
-                    }
-                }
-                true
-            }
-            selectedItemId = R.id.item_map
-        }
-    }
+//    fun initNavigationBar() {
+//        binding.bottomNavigation.run {
+//            setOnItemSelectedListener {
+//                when (it.itemId) {
+//                    R.id.item_list -> {
+//                        changeFragment(restaurant_list())
+//                    }
+//                    R.id.item_map -> {
+//                        changeFragment(google_map())
+//                    }
+//                }
+//                true
+//            }
+//            selectedItemId = R.id.item_map
+//        }
+//    }
+//
+//    fun changeFragment(fragment: Fragment) {
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(binding.navHostFragment.id , fragment)
+//            .commit()
+//    }
 
-    fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(binding.mapview.id , fragment)
-            .commit()
-    }
 }
